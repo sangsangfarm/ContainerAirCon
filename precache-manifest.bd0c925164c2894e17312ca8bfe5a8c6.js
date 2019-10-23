636dac36855607610dd69d9c9b9c3884b9b692f4
Janghun Lee <flacks12@gmail.com>
2019-10-23 09:16:20 +0900

    Updates

---
+++ precache-manifest.bd0c925164c2894e17312ca8bfe5a8c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822699a452bf3a4626e82ce72c76cf1b",
+    "url": "/sangsangfarm/ContainerAirCon/index.html"
+  },
+  {
+    "revision": "e0fb3ebca29c7475bea4",
+    "url": "/sangsangfarm/ContainerAirCon/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2b71526f3fcb4e980ece",
+    "url": "/sangsangfarm/ContainerAirCon/static/js/2.955a22bd.chunk.js"
+  },
+  {
+    "revision": "e0fb3ebca29c7475bea4",
+    "url": "/sangsangfarm/ContainerAirCon/static/js/main.b679b75c.chunk.js"
+  },
+  {
+    "revision": "3945982b2e46ccb40e19",
+    "url": "/sangsangfarm/ContainerAirCon/static/js/runtime-main.1a3d60bb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sangsangfarm/ContainerAirCon/static/media/logo.25bf045c.svg"
+  }
+]);
